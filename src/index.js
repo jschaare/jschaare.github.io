8564fbde3cb22bd2bd1cc4a3e611946ff093bf25
Justin Schaare <jschaare16@gmail.com>
2021-05-20 01:13:27 +0000

    add cname stuff

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const StrictModeApp = () => (
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+ReactDOM.render(<StrictModeApp />, document.getElementById("root"));
